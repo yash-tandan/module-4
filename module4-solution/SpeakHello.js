2920360dffb46969e7837176d5193f3938c1f5ac
yash-tandan <92167538+yash-tandan@users.noreply.github.com>
2021-11-25 08:52:29 +0530

    Add files via upload

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,19 @@
+// (function (window){
+//   var helloSpeaker= {};
+//   var speakWord = "Hello";
+//   helloSpeaker.speak = function(name) {
+//     console.log(speakWord + " " + name);
+//   }
+
+//   window.helloSpeaker = helloSpeaker;
+// })(window);
+
+(function (window) {
+  var speakWord = "Hello";
+  var helloSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.helloSpeaker = helloSpeaker;
+})(window);
